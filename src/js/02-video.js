b04162c469733aaa29915f424522fd47d729e9ac
Zhanna55 <zhannafesyk@gmail.com>
2022-11-13 22:30:22 +0200

    update videoplayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,29 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const playerEl = document.querySelector('#vimeo-player');
+
 const player = new Player(playerEl);
 
-player.on('timeupdate', function (timeupdate) {
-  console.log(timeupdate);
+function setTimeupdate(timeupdate) {
   return localStorage.setItem(
     'videoplayer-current-time',
     JSON.stringify({ timeupdate })
   );
-});
+}
+player.on('timeupdate', throttle(setTimeupdate, 1000));
+
 const currentTime = localStorage.getItem('videoplayer-current-time');
 const parseCurrentTime = JSON.parse(currentTime);
 const seconds = parseCurrentTime.timeupdate.seconds;
-player
-  .setCurrentTime(seconds)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+player.setCurrentTime(seconds).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      // the time was less than 0 or greater than the video’s duration
+      break;
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+    default:
+      // some other error occurred
+      break;
+  }
+});
