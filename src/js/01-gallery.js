4ba81094b986a8179d30b41d4194b2cb47b74905
Zhanna55 <zhannafesyk@gmail.com>
2022-11-13 03:31:06 +0200

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+
+const markup = createGalaryItems(galleryItems);
+
+listEl.insertAdjacentHTML('beforeend', markup);
+
+function createGalaryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      title="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery__item');
+
+gallery.on('show.simplelightbox', function () {
+  gallery.options.captionDelay = 250;
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+const playerEl = document.querySelector('#vimeo-player');
+const player = new Player(playerEl);
 
+player.on('timeupdate', function (timeupdate) {
+  console.log(timeupdate);
+  return localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify({ timeupdate })
+  );
+});
+const currentTime = localStorage.getItem('videoplayer-current-time');
+const parseCurrentTime = JSON.parse(currentTime);
+const seconds = parseCurrentTime.timeupdate.seconds;
+player
+  .setCurrentTime(seconds)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
