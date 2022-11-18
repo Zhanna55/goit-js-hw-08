8bb9eabff179cce780daeea38308f465d2a9c901
Zhanna55 <zhannafesyk@gmail.com>
2022-11-18 05:55:04 +0200

    update formData

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  name: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
 
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', setFormData);
+
+function setFormData(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+populeteForm();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function populeteForm() {
+  const data = localStorage.getItem('feedback-form-state');
+  if (data) {
+    const parseData = JSON.parse(data);
+    refs.name.value = parseData.email;
+    refs.message.value = parseData.message;
+  }
+}
