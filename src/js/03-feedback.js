15cfad958b4255be17ce263e05bf9048e06a7af8
Zhanna55 <zhannafesyk@gmail.com>
2022-11-18 20:42:23 +0200

    update formData

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,61 @@
+import throttle from 'lodash.throttle';
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   name: document.querySelector('.feedback-form input'),
   message: document.querySelector('.feedback-form textarea'),
 };
 
-const formData = {};
+let formData = populateFormData();
 
+refs.form.addEventListener('input', throttle(setFormData, 500));
 refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', setFormData);
 
 function setFormData(event) {
   formData[event.target.name] = event.target.value;
+
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
-
-populeteForm();
+populateForm();
 
 function onFormSubmit(event) {
   event.preventDefault();
+
+  if (refs.form.email.value === '' || refs.form.message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
   event.currentTarget.reset();
+  console.log(formData);
+  formData = {};
   localStorage.removeItem('feedback-form-state');
 }
 
-function populeteForm() {
+function populateForm() {
   const data = localStorage.getItem('feedback-form-state');
   if (data) {
-    const parseData = JSON.parse(data);
-    refs.name.value = parseData.email;
-    refs.message.value = parseData.message;
+    try {
+      const parseData = JSON.parse(data);
+      refs.name.value = parseData.email || '';
+      refs.message.value = parseData.message || '';
+    } catch (error) {
+      console.log(error.name);
+      console.log(error.message);
+    }
   }
+}
+function populateFormData() {
+  const data = localStorage.getItem('feedback-form-state');
+  const formData = {};
+  if (data) {
+    try {
+      const parseData = JSON.parse(data);
+      formData.email = parseData.email || '';
+      formData.message = parseData.message || '';
+    } catch (error) {
+      console.log(error.name);
+      console.log(error.message);
+    }
+  }
+  return formData;
 }
